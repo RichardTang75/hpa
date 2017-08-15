9d36757a838e209fcd92fb17baff61c235a5257f
RichardTang75 <richardtang75@gmail.com>
2017-08-15 02:06:05 -0400

    Add files via upload

--- take2 copy 3.cpp
+++ take2 copy 3.cpp
@@ -6,13 +6,6 @@
 //  Created by asdfuiop on 6/10/17.
 //  Copyright © 2017 asdfuiop
 
-//
-//          Copyright Richard Tang 2017.
-// Distributed under the Boost Software License, Version 1.0.
-//    (See accompanying file LICENSE_1_0.txt or copy at
-//          http://www.boost.org/LICENSE_1_0.txt)
-//
-
 #include <iostream>
 #include <string>
 #include <random>
