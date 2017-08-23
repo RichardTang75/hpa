1ecbdc53c8a70651ce8b6c51b21d63787819b1bc
RichardTang75 <richardtang75@gmail.com>
2017-08-23 11:58:04 -0400

    Add files via upload

---
+++ test_init.hpp
@@ -0,0 +1,14 @@
+//
+//  test_init.hpp
+//  take2
+//
+//  Created by asdfuiop on 8/20/17.
+//  Copyright © 2017 asdfuiop. All rights reserved.
+//
+
+#ifndef test_init_hpp
+#define test_init_hpp
+
+void map_stuff (int width, int height, vectormap& bigmap);
+
+#endif /* test_init_hpp */
